e8e65644d655c2d9f20155784661a4329a4cb589
Centuriononon <centuriononon@gmail.com>
2023-11-11 21:19:45 +0400

    chore: implemented graph repo model

--- lib/oh_my_adolf/graph_repo.ex
+++ lib/oh_my_adolf/graph_repo.ex
@@ -1,25 +1,111 @@
 defmodule OhMyAdolf.GraphRepo do
   @moduledoc """
-  This is a model over Bolt.Sips for executing queries over the interface.
+  This is a model over Bolt.Sips for executing queries over the URL based interface.
   """
-  # alias Bolt.Sips, as: Neo
+  require Logger
+  alias Bolt.Sips, as: Neo
 
   # def transaction(func), do: Neo.transaction(Neo.conn(), func)
   def transaction(func), do: func.()
 
-  def get_shortest_path(%URI{} = _s, %URI{} = _e) do
-  #   s_str = URI.to_string(s)
-  #   e_str = URI.to_string(e)
+  def get_shortest_path(%URI{} = start_url, %URI{} = end_url) do
+    s_str = URI.to_string(start_url)
+    e_str = URI.to_string(end_url)
+    s_hash = enc(s_str)
+    e_hash = enc(e_str)
 
-  #   Neo.conn()
-  #   |> Neo.query("""
-  #     MATCH path = shortestPath((start:LABEL {url: '#{s_str}'})-[:REFERS_TO*]-(end:LABEL {url: #{e_str}}))
-  #     RETURN path;
-  #   """)
-  {:error, "not found"} # or {:ok, [URI.t()]}
+    Neo.conn()
+    |> Neo.query("""
+      MATCH
+        (start:Page {url_hash: '#{s_hash}'}),
+        (end:Page {url_hash: '#{e_hash}'}),
+        path = shortestPath((start)-[:REFERS_TO*]-(end))
+      RETURN path;
+    """)
+    |> case do
+      {:error, %Neo.Error{message: m, code: c}} ->
+        Logger.critical(
+          "Could not fetch the shortest path;" <>
+            " error code: #{inspect(c)}, message: #{inspect(m)}"
+        )
+
+        {:error, "not found"}
+
+      {:ok, resp} ->
+        Logger.info(
+          "Fetched the shortest path;" <>
+            " records info: #{inspect(resp.records)}"
+        )
+
+        path =
+          resp
+          |> Map.get(:results)
+          |> Enum.at(0)
+          |> Map.get("path")
+          |> Neo.Types.Path.graph()
+          |> Stream.filter(fn
+            %Neo.Types.Node{} -> true
+            _ -> false
+          end)
+          |> Stream.map(fn node -> node.properties["url_hash"] end)
+          |> Stream.map(&dec/1)
+          |> Enum.map(&URI.parse/1)
+
+        {:ok, path}
+    end
   end
 
-  def register_path(_urls) do
-    :ok
+  def relate_pages(%URI{} = abv_url, %URI{} = sub_url) do
+    a_str = URI.to_string(abv_url)
+    s_str = URI.to_string(sub_url)
+    a_hash = enc(a_str)
+    s_hash = enc(s_str)
+
+    Neo.conn()
+    |> Neo.query("""
+      MERGE (above:Page {url_hash: '#{a_hash}'})
+      MERGE (sub:Page {url_hash: '#{s_hash}'})
+      MERGE (above)-[:REFERS_TO]->(sub) RETURN above, sub;
+    """)
+    |> case do
+      {:error, %Neo.Error{message: m, code: c}} ->
+        Logger.critical(
+          "Could not relate pages;" <>
+            " error code: #{inspect(c)}, message: #{inspect(m)}"
+        )
+
+        {:error, "query error"}
+
+      {:ok, resp} ->
+        Logger.info("Related pages; records info: #{inspect(resp.records)}")
+
+        {:ok}
+    end
   end
+
+  def exists?(%URI{} = url) do
+    u_str = URI.to_string(url)
+    u_hash = enc(u_str)
+
+    Neo.conn()
+    |> Neo.query("""
+      MATCH (p:Page {url_hash: '#{u_hash}'})
+      RETURN COUNT(p) > 0 AS exists
+    """)
+    |> case do
+      {:error, %Neo.Error{message: m, code: c}} ->
+        Logger.critical(
+          "Could check page for existence;" <>
+            " error code: #{inspect(c)}, message: #{inspect(m)}"
+        )
+
+        {:error, "query error"}
+
+      {:ok, resp} ->
+        Enum.at(resp.results, 0)["exists"]
+    end
+  end
+
+  defp dec(url) when is_bitstring(url), do: Base.decode64!(url)
+  defp enc(url) when is_bitstring(url), do: Base.encode64(url)
 end
