4d6446ac48166141c43a98f846c3dc33b07e26f4
Centuriononon <centuriononon@gmail.com>
2023-11-20 06:17:13 +0400

    test: added tests for the fetcher

--- test/oh_my_adolf/wiki/fetcher_test.exs
+++ test/oh_my_adolf/wiki/fetcher_test.exs
@@ -5,6 +5,8 @@ defmodule OhMyAdolf.Wiki.FetcherTest do
   alias OhMyAdolf.Wiki.Fetcher
   alias OhMyAdolf.HTTPClientMock
 
+  alias OhMyAdolf.Wiki.{BadResponseError, BadRequestError}
+
   @options Application.compile_env!(:oh_my_adolf, [:wiki, :http_options])
   @headers Application.compile_env!(:oh_my_adolf, [:wiki, :http_headers])
 
@@ -40,6 +42,47 @@ defmodule OhMyAdolf.Wiki.FetcherTest do
       end)
 
       Fetcher.fetch(url)
+    end
+  end
+
+  describe "Wiki.Fetcher fetch_page/1" do
+    test "should extract page on 200 status code" do
+      url = URI.parse("http://host.foo/path")
+      url_str = URI.to_string(url)
+      body = "apples"
+
+      expect(HTTPClientMock, :get, fn ^url_str, _headers, _options ->
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}}
+      end)
+
+      assert {:ok, ^body} = Fetcher.fetch_page(url)
+    end
+
+    test "should except non-200 status code" do
+      url = URI.parse("http://host.foo/path")
+      url_str = URI.to_string(url)
+
+      status_codes = [100, 201, 203, 404, 500]
+
+      for status <- status_codes do
+        expect(HTTPClientMock, :get, fn ^url_str, _headers, _options ->
+          {:ok, %HTTPoison.Response{status_code: status, body: "body"}}
+        end)
+
+        assert {:error, %BadResponseError{url: ^url, status_code: ^status}} = Fetcher.fetch_page(url)
+      end
+    end
+
+    test "should except httpoison error" do
+      url = URI.parse("http://host.foo/path")
+      url_str = URI.to_string(url)
+      reason = :timeout
+
+      expect(HTTPClientMock, :get, fn ^url_str, _headers, _options ->
+        {:error, %HTTPoison.Error{reason: reason}}
+      end)
+
+      assert {:error, %BadRequestError{url: ^url, reason: ^reason}} = Fetcher.fetch_page(url)
     end
   end
 end
