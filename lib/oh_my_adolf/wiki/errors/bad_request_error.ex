f6876f89b914f27ce24c6854794efd49d8fbc351
Centuriononon <centuriononon@gmail.com>
2023-11-20 06:17:39 +0400

    chore: updated errors

--- lib/oh_my_adolf/wiki/errors/bad_request_error.ex
+++ lib/oh_my_adolf/wiki/errors/bad_request_error.ex
@@ -1,9 +1,10 @@
 defmodule OhMyAdolf.Wiki.BadRequestError do
-  defexception [:message]
+  defexception [:message, :url, :reason]
 
   @impl true
-  def exception(url: %URI{} = url, reason: reason) when is_bitstring(reason) do
+  def exception(url: %URI{} = url, reason: reason)
+      when is_bitstring(reason) or is_atom(reason) do
     msg = "Could not request #{url} due to #{reason}"
-    %__MODULE__{message: msg}
+    %__MODULE__{message: msg, url: url, reason: reason}
   end
 end
--- lib/oh_my_adolf/wiki/errors/bad_response_error.ex
+++ lib/oh_my_adolf/wiki/errors/bad_response_error.ex
@@ -1,8 +1,8 @@
 defmodule OhMyAdolf.Wiki.BadResponseError do
-  defexception [:message]
+  defexception [:message, :status_code, :url]
 
   def exception(url: %URI{} = url, status: status) when is_number(status) do
     msg = "Received status code #{status} requesting #{url}"
-    %__MODULE__{message: msg}
+    %__MODULE__{message: msg, url: url, status_code: status}
   end
 end
